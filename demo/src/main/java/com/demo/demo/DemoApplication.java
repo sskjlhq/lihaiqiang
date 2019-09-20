e1a4a3b4342ca9fe92d5e29f99cec4ea4c049c0e
lihaiqiang <2496698053@qq.com>
2019-09-20 17:10:26 +0800

    hello world

--- demo/src/main/java/com/demo/demo/DemoApplication.java
+++ demo/src/main/java/com/demo/demo/DemoApplication.java
@@ -2,12 +2,19 @@ package com.demo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
-@SpringBootApplication
+/**
+ * springboot启动类
+ * @SpringBootApplication (后面括号内为去掉访问项目时的登录验证)
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, SecurityAutoConfiguration.class})
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
