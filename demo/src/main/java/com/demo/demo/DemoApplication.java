123a9553bb9e7ed5a6153d3cf3185eeed0802334
lihaiqiang <2496698053@qq.com>
2019-09-20 17:29:22 +0800

    启动类添加注释

--- demo/src/main/java/com/demo/demo/DemoApplication.java
+++ demo/src/main/java/com/demo/demo/DemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration
 /**
  * springboot启动类
  * @SpringBootApplication (后面括号内为去掉访问项目时的登录验证)
+ * springboot默认打包成jar包，可以通过cmd命令窗口 执行 java -jar jar包文件名  来运行程序
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, SecurityAutoConfiguration.class})
 public class DemoApplication {
--- demo/src/main/java/com/demo/demo/controller/controller.java
+++ demo/src/main/java/com/demo/demo/controller/controller.java
@@ -1,5 +1,6 @@
 package com.demo.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @date
  * @return
  */
+@Slf4j
 @Controller
 @RequestMapping("/")
 public class controller {
