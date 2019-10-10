e220bed74c9648288db35634ca29dd48160b516d
lihaiqiang <2496698053@qq.com>
2019-10-10 17:29:27 +0800

    添加测试类

---
+++ demo/src/test/java/com/demo/demo/Test.java
@@ -0,0 +1,24 @@
+package com.demo.demo;/**
+ * @description
+ * @author 李海强
+ * @date ${date} ${time}
+ */
+
+import java.util.HashMap;
+import java.util.Hashtable;
+
+/**
+ * @author 李海强
+ * @description 测试类
+ * @date 2019/10/10 16:45
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        long round = Math.round(-1.5);
+        System.out.println(round);
+        String a = "300";
+        System.out.println(Integer.valueOf(a));
+
+    }
+}
