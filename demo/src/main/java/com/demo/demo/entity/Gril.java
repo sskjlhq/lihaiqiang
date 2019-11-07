ba06dae7945de24877e66d49c90fafa03dee87cc
lihaiqiang <2496698053@qq.com>
2019-11-07 14:22:49 +0800

    修改实体类

--- demo/src/main/java/com/demo/demo/entity/Gril.java
+++ demo/src/main/java/com/demo/demo/entity/Gril.java
@@ -15,9 +15,12 @@ import java.io.Serializable;
  */
 @Data
 public class Gril implements Serializable {
+    // 主键id
     @Id
     @GeneratedValue(generator = "system-uuid")
     private String id;
+    // 姓名
     private String name;
+    // 年龄
     private String age;
 }
