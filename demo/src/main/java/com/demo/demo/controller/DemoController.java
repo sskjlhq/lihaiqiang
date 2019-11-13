9985de3365cef1996712dec43c7a87eb8a72d24e
lihaiqiang <2496698053@qq.com>
2019-11-13 19:43:40 +0800

    demo controller

---
+++ demo/src/main/java/com/demo/demo/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.demo.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author 李海强
+ * @description demo controller
+ * @date 2019/11/13 19:42
+ */
+@Controller
+public class DemoController {
+}
